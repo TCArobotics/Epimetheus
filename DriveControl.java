6ce2932b245ba8f61380bfbcd6c86e2518451e21
TCArobotics <61090685+TCArobotics@users.noreply.github.com>
2021-03-17 14:11:32 -0600

    Add files via upload

--- DriveControl.java
+++ DriveControl.java
@@ -32,6 +32,9 @@ public class DriveControl
     private final double kdistanceBtwnWheels = .557; //Distance between wheels in meters
     private final double kleftMultiplier = 1; //Value to multiply left speed by
     private final double krightMultiplier = 1; //Value to multiply right speed by
+    private final Double[] barrelTimings = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0};    //stores how long each step of autonomous takes. (fine tune)
+    private final Double[] slalomTimings = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0};
+    private final Double[] bounceTimings = {5.0, 10.0, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0};
 
     //Variables that do not have default values
     private double LeftDriveInput; //Stores the actual left input value for execute
@@ -45,7 +48,6 @@ public class DriveControl
     //initialize variables
     public DriveControl(XboxController _XboxController)
     {
-        
         m_rearLeft = new PWMVictorSPX(RobotMap.kRearLeftPort);
         m_frontRight = new PWMVictorSPX(RobotMap.kFrontRightPort);
         m_rearRight = new PWMVictorSPX(RobotMap.kRearRightPort);
@@ -92,10 +94,10 @@ public class DriveControl
     }
 
     //Calculates motor speeds for the robot to drive in a straight line
-    public void calculateAutonomousDrive(double _currentTime, double _startTime, double _duration, double _rightInput, double _leftInput, double _speed)
+    public void calculateAutonomousDrive(double _currentTime, double _startTime, double _endTime, double _rightInput, double _leftInput, double _speed)
     {
     	//Calculates whether the time span is correct for the function to run
-    	if ((_currentTime >= _startTime) && (_currentTime <= _startTime + _duration))
+    	if ((_currentTime >= _startTime) && (_currentTime <= _endTime))
         {
             this.selectedDrive = 2;
             this.RightDriveInput = _rightInput;
@@ -105,10 +107,10 @@ public class DriveControl
     }  
 
     //Calculates motor speeds for the robot to drive in a circle (radius = inner wheel to point of rotation)
-    public void calculateAutonomousCircle(double _currentTime, double _startTime, double _duration, double _speed, double _radius, boolean _isClockwise)
+    public void calculateAutonomousCircle(double _currentTime, double _startTime, double _endTime, double _speed, double _radius, boolean _isClockwise)
     {
         //Calculates whether the time span is correct for the function to run
-        if ((_currentTime >= _startTime) && (_currentTime <= _startTime + _duration))
+        if ((_currentTime >= _startTime) && (_currentTime <= _endTime))
             {
                 double circleCalc = _radius / (_radius + kdistanceBtwnWheels);
                 this.selectedDrive = 2;
@@ -142,25 +144,25 @@ public class DriveControl
     {
         m_autoTimerCurrent = Timer.getFPGATimestamp();
         //forward for 7.5 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 0, 5 , 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, 0, barrelTimings[0], 1.0, 1.0, -0.5);
 
         //drive in a circle with (radius (3ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 5, 5, -0.5, 3, true);
+        calculateAutonomousCircle(m_autoTimerCurrent, barrelTimings[0], barrelTimings[1], -0.5, 3, true);
     
         //forward for 9 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 10, 5 , 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, barrelTimings[1], barrelTimings[2], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (3ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 15, 5, -0.5, 3, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, barrelTimings[2], barrelTimings[3], -0.5, 3, false);
     
         //forward for 7 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 20, 5 , 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, barrelTimings[3], barrelTimings[4], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (3ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 15, 5, -0.5, 3, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, barrelTimings[4], barrelTimings[5], -0.5, 3, false);
     
         //forward for 20 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 30, 5 , 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, barrelTimings[5], barrelTimings[6], 1.0, 1.0, -0.5);
     
         //execute function
         execute();
@@ -170,73 +172,73 @@ public class DriveControl
     {
         m_autoTimerCurrent = Timer.getFPGATimestamp();
         //drive in a circle with (radius (0ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 0, 5, -0.5, 0, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, 0, slalomTimings[0], -0.5, 0, false);
 
         //forward for 7 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 5, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, slalomTimings[0], slalomTimings[1], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (5ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 10, 5, -0.5, 5, true);
+        calculateAutonomousCircle(m_autoTimerCurrent, slalomTimings[1], slalomTimings[2], -0.5, 5, true);
     
         //forward for 7 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 15, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, slalomTimings[2], slalomTimings[3], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (2.5ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 20, 5, -0.5, 2.5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, slalomTimings[3], slalomTimings[4], -0.5, 2.5, false);
     
         //forward for 7 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 25, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, slalomTimings[4], slalomTimings[5], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (5ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 30, 5, -0.5, 5, true);
+        calculateAutonomousCircle(m_autoTimerCurrent, slalomTimings[5], slalomTimings[6], -0.5, 5, true);
     
         //forward for 7 ft
-        calculateAutonomousDrive(m_autoTimerCurrent, 35, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, slalomTimings[6], slalomTimings[7], 1.0, 1.0, -0.5);
     
         //drive in a circle with (radius (0ft), speed (-0.5))
-        calculateAutonomousCircle(m_autoTimerCurrent, 40, 5, -0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, slalomTimings[7], slalomTimings[8], -0.5, 5, false);
     }
 
     public void bouncePath()
     {
         m_autoTimerCurrent = Timer.getFPGATimestamp();
         //Turn Left to face cone
-        calculateAutonomousCircle(m_autoTimerCurrent, 0, 1, -0.5, 3, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, 0, bounceTimings[0], -0.5, 3, false);
 
         //Drive into cone A3
-        calculateAutonomousDrive(m_autoTimerCurrent, 1, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[0], bounceTimings[1], 1.0, 1.0, -0.5);
     
         //Turn Left to face back away from cone while heading backwards
-        calculateAutonomousCircle(m_autoTimerCurrent, 6, 2, -0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[1], bounceTimings[2], -0.5, 5, false);
     
         //Drive Forward
-        calculateAutonomousDrive(m_autoTimerCurrent, 8, 3, -1.0, -1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[2], bounceTimings[3], -1.0, -1.0, -0.5);
     
         //Circle Around D5 CCW
-        calculateAutonomousCircle(m_autoTimerCurrent, 11, 5, 0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[3], bounceTimings[4], 0.5, 5, false);
     
         //Head into cone A6
-        calculateAutonomousDrive(m_autoTimerCurrent, 16, 5, -1.0, -1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[4], bounceTimings[5], -1.0, -1.0, -0.5);
     
         //Turn Left to face back away from cone while heading backwards
-        calculateAutonomousCircle(m_autoTimerCurrent, 21, 2, -0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[5], bounceTimings[6], -0.5, 5, false);
     
         //Drive Forward
-        calculateAutonomousDrive(m_autoTimerCurrent, 23, 3, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[6], bounceTimings[7], 1.0, 1.0, -0.5);
     
         //Turn around D7 CCW
-        calculateAutonomousCircle(m_autoTimerCurrent, 26, 2, 0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[7], bounceTimings[8], 0.5, 5, false);
     
         //Head forwards past D8
-        calculateAutonomousDrive(m_autoTimerCurrent, 28, 4, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[8], bounceTimings[9], 1.0, 1.0, -0.5);
     
         //Turn around D8 CCW
-        calculateAutonomousCircle(m_autoTimerCurrent, 30, 2, 0.5, 5, false);
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[9], bounceTimings[10], 0.5, 5, false);
     
         //Head into cone A9
-        calculateAutonomousDrive(m_autoTimerCurrent, 32, 5, 1.0, 1.0, -0.5);
+        calculateAutonomousDrive(m_autoTimerCurrent, bounceTimings[10], bounceTimings[11], 1.0, 1.0, -0.5);
     
         //Circle into finish zone
-        calculateAutonomousCircle(m_autoTimerCurrent, 37, 4, -0.5, 15, false);    
+        calculateAutonomousCircle(m_autoTimerCurrent, bounceTimings[11], bounceTimings[12], -0.5, 15, false);    
     }
 }
